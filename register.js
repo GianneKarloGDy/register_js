67ec3722e2576961d6fceaee9a40a5b134114e7f
Kurt Isiah S. Santillana <euchelleignacio36@gmail.com>
2024-03-05 14:32:24 +0800

    added password and confirm password in register.html and register.js

--- register.js
+++ register.js
@@ -7,12 +7,28 @@
 //console.log(typeof(name))
 
 let txtUsername = document.querySelector("#txtUsername")
+let txtPassword = document.querySelector("#txtPassword")
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword")
 let btnRegister = document.querySelector("#btnRegister")
 
 btnRegister.onclick = function () {
-    register(txtUsername.value)
+    register(txtUsername.value, txtPassword.value, txtConfirmPassword.value)
 }
 
-function register (username) {
-    console.log(username)
+function register (username, password, confirmPassword) {
+    // if... else if .... else
+    // Conditional Operators (Greater than >, Less than <, Equal ==, Not !, or ||)
+    // Greater Than or Equal >=, Less Than or Equal <=
+    // Not Equal !=
+
+    console.log(username, password, confirmPassword)
+
+    if(password == confirmPassword && password != "" && confirmPassword != "") {
+        return console.log("Password match!")
+    } else if (password == "" || confirmPassword == ""){
+        return console.log("Password must not be empty")
+    }else {
+        return console.log("Password does not match!")
+    }
+    console.log(username, password, confirmPassword)
 }
